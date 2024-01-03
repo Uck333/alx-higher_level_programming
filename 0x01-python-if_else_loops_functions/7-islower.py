95dbc7175664bdfce47fbbf66876dff0296e1952
uck <uzohk737@gmail.com>
2024-01-03 17:02:57 +0000

    0. Positive anything is better than negative nothing

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - nil to declare
+ * @void - nil
+ *
+ * Description: c program for alpha
+ * Return: nil if successful
+ */
+int main(void)
+{
+	char z;
+
+	for (z = 'z'; z >= 'a'; z--)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
