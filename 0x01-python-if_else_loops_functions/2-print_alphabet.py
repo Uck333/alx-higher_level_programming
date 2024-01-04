a09b4b2033eb2d247ad04da01fd33a6151b75004
uck <uzohk737@gmail.com>
2024-01-04 00:53:41 +0000

    repeat

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+print("".join(chr(i) for i in range(ord('a'), ord('z') + 1)), end="")
+
+# VERSION1
 # for i in range(97, 123):
 #    print(chr(i), end="")
 
 # VERSION2
-for i in range(97, 123):
-    print("{:c}".format(i), end="")
+# for i in range(97, 123):
+#    print("{:c}".format(i), end="")
 
 # VERSION3
 # import string
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i in [101, 113]:
-        continue
-    print("{:c}".format(i), end="")
+print(*["%c" % a for a in range(ord('a'), ord('z') + 1)
+        if a not in (ord('q'), ord('e'))], sep='', end='')
+# VERSION1
+# for i in range(97, 123):
+#    if i in [101, 113]:
+#        continue
+#    print("{:c}".format(i), end="")
 
 # VERSION2
 # for i in range(97, 123)
