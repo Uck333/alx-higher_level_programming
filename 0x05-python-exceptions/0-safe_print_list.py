9cf6795867cae85bb6445824e5ca8582e1392b8f
uck <uzohk737@gmail.com>
2024-01-22 15:52:09 +0000

    0. Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    k = 0
+    try:
+        for _ in my_list:
+            j = j + 1
+        for _ in my_list[:x]:
+            k = k + 1
+            print(_, end="\n")
+        if k <= j:
+            return k
+    except ValueError:
+        print("Omg! There are more elements than my_list contains.")
+
+
+#VERSION 2
+#def safe_print_list(my_list=[], x=0):
+#    j = 0
+#    k = 0
+#    try:
+#        for i in range (len(my_list[])):
+#            j = j + 1
+#        for x in range (len(my_list[])):
+#            k = k + 1
+#            print(my_list[x], end="\n")
+#        if k <= j:
+#            return k
+#    except ValueError:
+#        print("Omg! There are more elements than my_list contains.")
